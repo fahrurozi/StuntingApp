5d7ca5663de7296734ce9f8b6acf7624cc203a0b
zerodev <akbarabdul80@gmail.com>
2022-06-06 16:33:42 +0700

    Fix Home add setText name & username

--- app/src/main/java/com/example/stunting/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/stunting/ui/home/HomeFragment.java
@@ -1,28 +1,43 @@
 package com.example.stunting.ui.home;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.stunting.BuildConfig;
 import com.example.stunting.R;
 import com.example.stunting.ui.info.StuntingInfoActivity;
 import com.example.stunting.ui.stunting_map.StuntingMapActivity;
 
 public class HomeFragment extends Fragment {
 
+    private SharedPreferences sharedPref;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Button btnInfo = view.findViewById(R.id.btn_goto_info);
         LinearLayout btnStuntingMap = view.findViewById(R.id.btn_stunting_map);
+
+        //Session
+        sharedPref = getContext().getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
+
+        TextView tvName = view.findViewById(R.id.tvName);
+        TextView tvUsername = view.findViewById(R.id.tvUsername);
+
+        tvName.setText(sharedPref.getString(getString(R.string.name), ""));
+        tvUsername.setText(sharedPref.getString(getString(R.string.username), ""));
 
         btnInfo.setOnClickListener(v ->
                 startActivity(new Intent(requireContext(), StuntingInfoActivity.class))
