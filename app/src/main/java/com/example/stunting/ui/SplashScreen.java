5e152e54305fa0b512caabd58d331c0186229bc8
zerodev <akbarabdul80@gmail.com>
2022-06-06 16:24:22 +0700

    Fix SplashScreen jika sudah login masuk ke MainActivity

--- app/src/main/java/com/example/stunting/ui/SplashScreen.java
+++ app/src/main/java/com/example/stunting/ui/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.example.stunting.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
@@ -10,18 +12,24 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.stunting.BuildConfig;
 import com.example.stunting.R;
 
 public class SplashScreen extends AppCompatActivity {
     Animation app_splash, btt;
     ImageView app_logo;
     TextView app_subtitle;
-
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Session
+        sharedPref = this.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
 
         app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
         btt = AnimationUtils.loadAnimation(this, R.anim.btt);
@@ -35,9 +43,16 @@ public class SplashScreen extends AppCompatActivity {
         Handler handler = new Handler();
         handler.postDelayed(() -> {
             // merubah activity ke activity lain
-            Intent gogetstarted = new Intent(SplashScreen.this, GetStartedActivity.class);
-            startActivity(gogetstarted);
-            finish();
+            String ID = sharedPref.getString(getString(R.string.id), "");
+            if (ID.equals("")) {
+                Intent gogetstarted = new Intent(SplashScreen.this, GetStartedActivity.class);
+                startActivity(gogetstarted);
+                finish();
+            } else {
+                Intent gomain = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(gomain);
+                finish();
+            }
         }, 2000); // 2000 ms = 2s
     }
 }
